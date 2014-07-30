d8dd968cf5e3fe0c79e78fc8967dd4896399b78b
Adrian Pichardo <adrianpichardo@Adrians-MacBook-Pro.local>
2014-07-30 01:15:00 -0400

    Initial Commit

---
+++ Testing July292014/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Testing July292014
+//
+//  Created by Adrian Pichardo on 7/30/14.
+//  Copyright (c) 2014 AP inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
